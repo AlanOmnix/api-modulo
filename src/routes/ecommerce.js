b5303706f9221526dc4fe29cf4d93056187a95a9
= <alansm9790@gmail.com>
2022-04-21 08:04:08 -0500

    actionkubectl

--- src/routes/ecommerce.js
+++ src/routes/ecommerce.js
@@ -3,6 +3,10 @@ import { ecommerce } from '../controllers/ecommerce'
 
 const router = Router()
 
+router.get('/api/1.0/hello', (req, res)=>{
+    res.send('Funciona')
+})
+
 router.post('/api/1.0/initialreserve', ecommerce.initialReserve)
 
 router.post('/api/1.0/reservepayment', ecommerce.reservePayment)
