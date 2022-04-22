60105114ee6202b758f4a20f70f0b6120e4beb71
canielcm <ccastromaussa@gmail.com>
2022-04-22 13:00:42 -0500

    add route get

--- src/routes/ecommerce.js
+++ src/routes/ecommerce.js
@@ -2,11 +2,12 @@ import { Router } from 'express'
 import { ecommerce } from '../controllers/ecommerce'
 
 const router = Router()
-router.post('/api/1.0/hello', (req,res)=>{
+router.get('/api/1.0/hello', (req,res)=>{
     res.json({
         message: "hello world"
     })
 })
+
 router.post('/api/1.0/initialreserve', ecommerce.initialReserve)
 
 router.post('/api/1.0/reservepayment', ecommerce.reservePayment)
