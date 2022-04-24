0406e18718ddce729d0c1f14b06e6782a486a417
= <alansm9790@gmail.com>
2022-04-24 12:57:23 -0500

    jenkins

--- Jenkinsfile
+++ Jenkinsfile
@@ -15,6 +15,9 @@ node {
         nodejs(nodeJSInstallationName: 'nodejs'){
             sh "npm install"
             withSonarQubeEnv('sonar'){
+                sh "npm install --save-dev mocha chai"
+                sh "npm run test"
+                sh "npm run coverage-lcov"
                 sh "npm install sonar-scanner"
                 sh "npm run sonar"
             }
--- src/routes/ecommerce.js
+++ src/routes/ecommerce.js
@@ -2,15 +2,9 @@ import { Router } from 'express'
 import { ecommerce } from '../controllers/ecommerce'
 
 const router = Router()
-router.get('/api/1.0/hello', (req,res)=>{
-    res.json({
-        message: "hello"
-    })
-})
-router.get('/api/1.0/helloworld', (req,res)=>{
-    res.json({
-        message: "helloworld"
-    })
+
+router.get('/', (req,res)=>{
+    res.send('Api-Modulo')
 })
 
 router.post('/api/1.0/initialreserve', ecommerce.initialReserve)
